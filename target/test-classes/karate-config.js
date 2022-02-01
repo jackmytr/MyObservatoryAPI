814dfd33a1990239b192db59e0da87f08cc75265
jackmytr <85142838+jackmytr@users.noreply.github.com>
2022-02-01 18:41:22 +0800

    Comment review

--- target/test-classes/karate-config.js
+++ target/test-classes/karate-config.js
@@ -4,12 +4,19 @@ function fn() {
  if (!env) {
    env = 'IOS';
  }
+
+/*
+Define IOS API URL
+*/
  var config = {
    env: env,
    myVarName: 'MyObservatory',
    baseUrl: 'https://pda.weather.gov.hk/locspc/data/fnd_e.xml'   //9-Day forecast endpoint ENG - IOS
 }
 
+/*
+Define Android API URL
+*/
  if (env == 'Android') {
     config.baseUrl = 'https://pda.weather.gov.hk/locspc/android_data/fnd_e.xml' ;  //9-Day forecast endpoint ENG - Android
  }
